c8121a10be91ec1f80cba41bff826e1efdee2b8f
Mouhamed Lamine Ngom <ngomdxn@gmail.com>
2025-02-13 16:45:19 +0000

    Saving changes regarding issues with brand-section sensitive case

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c951d34020e9e5c22a57"; }
+/******/ 		__webpack_require__.h = function() { return "378b273742f5e7396839"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
